77553f3392da683dadfc2688b8b27dd9fe8ca787
Ferdouse H. Nowrin <84663223+nowrin-ux@users.noreply.github.com>
2023-04-16 17:10:26 +0600

    Add files via upload

---
+++ ternary op smart if-else.cpp
@@ -0,0 +1,21 @@
+
+// Ternary operator is the smart if else
+
+#include<iostream>
+using namespace std;
+
+int main () {
+
+int num ;
+cin >> num ;
+  (num % 2 == 0 ) ? cout << "Even"  :  cout << "Odd" <<endl ; // syntax : (condition) ? (value_if_true)  : (value_if_false) ;
+
+  return 0;
+
+}
+
+
+
+
+
+
